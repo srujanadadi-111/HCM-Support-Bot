cb0b791808b526c0a35bf47c253bdf49241f558e
srujanadadi-111 <srujana.dadi@egovernments.org>
2025-02-02 03:40:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,6 @@ import streamlit as st
 if 'question_clicks' not in st.session_state:
     st.session_state.question_clicks = {
         "What is Health Campaign Management?": 0,
-        "How do you do custom report generation?": 0,
         "What are the steps involved in creating a KPI?": 0
     }
 
